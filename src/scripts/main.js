f7c6790ba414cff341d18865e522cbc6d0a35163
Anton Talalaev <anton.talalaev@evotrux.com>
2020-12-08 09:29:28 -0600

    Adds Attendance button functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,7 @@ const addName = attendanceContainerEle.querySelector('button');
 const nameList = attendanceContainerEle.querySelector('ul');
 
 // === ADD SCRIPT FOR ADDING NAMES HERE === 
+addName.addEventListener('click', function(){
+    console.log('here');
+    nameList.insertAdjacentHTML ("afterbegin", "<li>Erin</li> <li>Phu</li>");
+})
